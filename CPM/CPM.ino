ba3a441712a0479d51097e0e21204766fe883d6b
choiyj0812 <cyjin1513@gmail.com>
2022-06-24 21:30:21 +0900

    CPM 0.6
    Change code from Socket.io

--- CPM/CPM.ino
+++ CPM/CPM.ino
@@ -1,119 +1,171 @@
+#include <DFPlayer_Mini_Mp3.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
-#include <DFPlayer_Mini_Mp3.h>
+#include <ArduinoJson.h>
+#include <WebSocketsClient.h>
+#include <SocketIOclient.h>
+#include <Hash.h>
 
-#define ssid "ssid"
-#define pswd "password"
+ESP8266WiFiMulti WiFiMulti;
+SocketIOclient socketIO;
+
+#define ssid "SSID"
+#define pswd "passpasspass"
+#define addr "10.11.100.100"
+#define port 8880
+#define URL "/socket.io/?EIO=4"
+
+#define USE_SERIAL Serial
+
+/*
+ * Socket.io Event list
+ * connect : 연결 시작
+ * error : 연결 오류
+ * disconnect : 연결 중단
+ * (추정)event : 통신을 통한 데이터 수신
+ * (추정)ACK : 데이터 송신 시 확인용으로 수신
+ */
+
+#define Time 10000
 
 #define SoundRx Rx
 #define SoundTx Tx
-#define Person 1
+#define Pir_Sensor 1
 #define LED1 2
 #define LED2 3
 int mp3_num = 1;
 int mp3_max = 5;
+int PIR;
+bool LED_switch = false;
 
-ESP8266WiFiMulti WiFiMulti;
+void socketIOEvent(socketIOmessageType_t type, uint8_t * payload, size_t length) {
+    switch(type) {
+        case sIOtype_DISCONNECT:
+            USE_SERIAL.printf("[Socket.IO] Disconnected\n");
+            break;
+        case sIOtype_CONNECT:
+            USE_SERIAL.printf("[Socket.IO] Connected to url : %s\n", payload);
 
-void setup() {
-
-  Serial.begin(115200);
-  // Serial.setDebugOutput(true);
-
-  mp3_set_serial(Serial);
-  mp3_set_volume(30);
-  delay(1);
-
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
-
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP(ssid, pswd);
-
+            // join default namespace (no auto join in Socket.IO V3)
+            // 기본 네임스페이스 설정
+            socketIO.send(sIOtype_CONNECT, "/");
+            break;
+        case sIOtype_EVENT:
+            USE_SERIAL.printf("[Socket.IO] get event : %s\n", payload);
+            if(payload == "ON") LED_switch = true;
+            else if(payload == "OFF") LED_switch = false;
+            break;
+        case sIOtype_ACK:
+            USE_SERIAL.printf("[Socket.IO] get ack : %u\n", length);
+            hexdump(payload, length);
+            break;
+        case sIOtype_ERROR:
+            USE_SERIAL.println("[Socket.IO] can connected");
+            USE_SERIAL.printf("            get error_code : %u\n", length);
+            hexdump(payload, length);
+            break;
+        case sIOtype_BINARY_EVENT:
+            USE_SERIAL.printf("[Socket.IO] get binary event : %u\n", length);
+            hexdump(payload, length);
+            break;
+        case sIOtype_BINARY_ACK:
+            USE_SERIAL.printf("[Socket.IO] get binary ack: %u\n", length);
+            hexdump(payload, length);
+            break;
+    }
 }
 
+void setup() {
+    USE_SERIAL.begin(115200);
+    USE_SERIAL.setDebugOutput(true);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+    // Serial에 데이터가 입력될 때까지 대기
+    for(uint8_t t = 4; t > 0; t--) {
+      USE_SERIAL.printf("[Serial] NOW BOOTING-%02d...\n", t);
+      USE_SERIAL.flush();
+      delay(1000);
+    }
+
+    // AP 비활성화
+    if(WiFi.getMode() & WIFI_AP) {
+      WiFi.softAPdisconnect(true);
+    }
+
+    // AP 추가
+    WiFiMulti.addAP(ssid, pswd);
+    USE_SERIAL.println("[WiFi] Add Access Point");
+
+    // WiFi가 연결되어있지 않으면 대기
+    while(WiFiMulti.run() != WL_CONNECTED) {
+      USE_SERIAL.println("[WiFi] Connecting...");
+      delay(100);
+    }
+
+    // IP를 문자열로 저장 후 출력
+    String ip = WiFi.localIP().toString();
+    USE_SERIAL.printf("[WiFi] Connected, IP : %s\n", ip.c_str());
+
+    // 소켓io를 주소, 포트, URL을 통해 연결
+    socketIO.begin(addr, port, URL);
+
+    // 이벤트가 발생하면 socketIOEvent() 실행
+    socketIO.onEvent(socketIOEvent);
+}
+
+unsigned long messageTimestamp = 0;
+unsigned long timeToPIR = 0;
 void loop() {
-  String payload = "";
-  static bool Mode = false;
-  
-  // wait for WiFi connection
-  // 와이파이 연결
-  if ((WiFiMulti.run() == WL_CONNECTED)) {
+    socketIO.loop();
 
-    WiFiClient client;
+    uint64_t Now = millis();
 
-    HTTPClient http;
-
-    Serial.print("[HTTP] begin...\n");
-    // 연결 성공 시
-    if (http.begin(client, "http://jigsaw.w3.org/HTTP/connection.html")) {  // HTTP
-
-
-      Serial.print("[HTTP] GET...\n");
-      // start connection and send HTTP header
-      // 서버와의 연결 시도 및 http 헤더 전송
-      int httpCode = http.GET();
-
-      // httpCode will be negative on error
-      // 에러가 나지 않는다면 httpcode는 양수
-      if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        // http 헤더가 전송된 뒤 서버응답 헤더가 처리됨
-        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-        // file found at server
-        // 서버에서 파일을 찾음
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          // 문자열을 받아옴
-          payload = http.getString();
-          Serial.println(payload);
-        }
+    if(Now - timeToPIR > Time){
+      timeToPIR = Now;
+      PIR = digitalRead(Pir_Sensor);
+      if(PIR){
+        mp3_play(mp3_num);
+        mp3_num++;
+        if(mp3_num > mp3_max) mp3_num = 1;
       }
-      // httpcode < 0
-      else {
-        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-      }
-
-      http.end();
     }
-    // 연결 실패 시
-    else {
-      Serial.printf("[HTTP} Unable to connect\n");
+
+    if(LED_switch){
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
     }
-  }
+    else{
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+    }
 
-  // 입력받은 문자열에 따라 Mode 변경
-  if(payload == "ON") Mode = true;
-  else if(payload == "OFF") Mode = false;
+    if(Now - messageTimestamp > Time) {
+      messageTimestamp = Now;
 
-  // Mode에 따라 LED를 조절
-  if(Mode){
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, HIGH);
-  }
-  else{
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, LOW);
-  }
+      // 소켓io를 위한 Json 메시지(String형) 생성
+      DynamicJsonDocument doc(1024);
+      JsonArray array = doc.to<JsonArray>();
 
-  // 사람이 감지되면 칭찬 재생
-  int PIR = digitalRead(Person);
-  if(PIR){
-    mp3_play(mp3_num);
-    mp3_num++;
-    if(mp3_num > mp3_max) mp3_num = 1;
-  }
+      // Json에 이벤트 이름 추가
+      array.add("event_name");
 
-  // 10초 대기
-  delay(10000);
+      // Json 객체에 문자열 추가
+      JsonObject param1 = array.createNestedObject();
+      param1["now"] = (uint32_t) Now;
+
+      // String에 Json 객체가 직렬화되어 저장
+      String output;
+      serializeJson(doc, output);
+
+      // 메시지 전송
+      socketIO.sendEVENT(output);
+
+      // Json 메시지 출력
+      USE_SERIAL.print("[Socket.IO] Send Message : ");
+      USE_SERIAL.println(output);
+    }
 }
