1e59d2fb6e69c7aff46b511e1ad44254fa7263a1
choiyj0812 <cyjin1513@gmail.com>
2022-06-27 17:19:10 +0900

    MTHM 0.3
    Change code with Socket.io

--- MTHM/MTHM.ino
+++ MTHM/MTHM.ino
@@ -1,94 +1,166 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFiMulti.h>
+#include <ArduinoJson.h>
+#include <WebSocketsClient.h>
+#include <SocketIOclient.h>
+#include <Hash.h>
 #include <DHT.h>
 
-#define SERVER_IP "192.168.1.42"
+ESP8266WiFiMulti WiFiMulti;
+SocketIOclient socketIO;
 
-#ifndef STASSID
-#define STASSID "ssid"
-#define STAPSWD "password"
-#endif
+#define ssid "SSID"
+#define pswd "passpasspass"
+#define addr "10.11.100.100"
+#define port 8880
+#define URL "/socket.io/?EIO=4"
+
+#define USE_SERIAL Serial
 
 #define SOIL_SENSOR 14
 #define DHTPIN 4
 #define DHTTYPE DHT11
+
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
+/*
+ * Socket.io Event list
+ * connect : 연결 시작
+ * error : 연결 오류
+ * disconnect : 연결 중단
+ * (추정)event : 통신을 통한 데이터 수신
+ * (추정)ACK : 데이터 송신 시 확인용으로 수신
+ */
 
-  Serial.begin(115200);
-  dht.begin();
+#define Time 10000
 
-  Serial.println();
-  Serial.println();
-  Serial.println();
+void socketIOEvent(socketIOmessageType_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case sIOtype_DISCONNECT:
+      USE_SERIAL.printf("[Socket.IO] Disconnected!\n");
+      break;
+    case sIOtype_CONNECT:
+      USE_SERIAL.printf("[Socket.IO] Connected to url: %s\n", payload);
 
-  WiFi.begin(STASSID, STAPSWD);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+      // join default namespace (no auto join in Socket.IO V3)
+      socketIO.send(sIOtype_CONNECT, "/");
+      break;
+    case sIOtype_EVENT:
+      USE_SERIAL.printf("[Socket.IO] get event: %s\n", payload);
+      break;
+    case sIOtype_ACK:
+      USE_SERIAL.printf("[Socket.IO] get ack: %u\n", length);
+      hexdump(payload, length);
+      break;
+    case sIOtype_ERROR:
+      USE_SERIAL.println("[Socket.IO] can connected");
+      USE_SERIAL.printf("            get error_code : %u\n", length);
+      hexdump(payload, length);
+      break;
+    case sIOtype_BINARY_EVENT:
+      USE_SERIAL.printf("[Socket.IO] get binary: %u\n", length);
+      hexdump(payload, length);
+      break;
+    case sIOtype_BINARY_ACK:
+      USE_SERIAL.printf("[Socket.IO] get binary ack: %u\n", length);
+      hexdump(payload, length);
+      break;
   }
-  Serial.println("");
-  Serial.print("Connected! IP address: ");
-  Serial.println(WiFi.localIP());
-
 }
 
+void setup() {
+  dht.begin();
+  USE_SERIAL.begin(115200);
+  USE_SERIAL.setDebugOutput(true);
+
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+
+  for(uint8_t t = 4; t > 0; t--) {
+    USE_SERIAL.printf("[Serial] BOOT WAIT %d...\n", t);
+    USE_SERIAL.flush();
+    delay(1000);
+  }
+
+  // AP 비활성화
+  if(WiFi.getMode() & WIFI_AP) {
+    WiFi.softAPdisconnect(true);
+  }
+
+  // AP 추가
+  WiFiMulti.addAP(ssid, pswd);
+  USE_SERIAL.println("[WiFi] Add Access Point");
+
+  // WiFi가 연결되어있지 않으면 대기
+  while(WiFiMulti.run() != WL_CONNECTED) {
+    USE_SERIAL.println("[WiFi] Connecting...");
+    delay(100);
+  }
+
+  // IP를 문자열로 저장 후 출력
+  String ip = WiFi.localIP().toString();
+  USE_SERIAL.printf("[WiFi] Connected, IP : %s\n", ip.c_str());
+
+  // 소켓io를 주소, 포트, URL을 통해 연결
+  socketIO.begin(addr, port, URL);
+
+  // 이벤트가 발생하면 socketIOEvent() 실행
+  socketIO.onEvent(socketIOEvent);
+}
+
+float Temp;
+int Soil_Data;
+
+unsigned long messageTimestamp = 0;
+unsigned long sensor_time = 0;
 void loop() {
-  float Temp_C = dht.readTemperature();
-  float Temp_F = dht.readTemperature(true);
-  int Soil_Data = analogRead(Soil_Sensor);
-  if(isnan(Temp_C) || isnan(Temp_F) || isnan(Soil_Data)){
-    Serial.println("Failed to read from DHT11 Sensor and Soil Sensor!");
-    return;
-  }
-  
-  // wait for WiFi connection
-  // 와이파이 연결 시도
-  // 와이파이 연결 성공 시 작동
-  if ((WiFi.status() == WL_CONNECTED)) {
+  socketIO.loop();
 
-    WiFiClient client;
-    HTTPClient http;
+  uint64_t now = millis();
 
-    Serial.print("[HTTP] begin\n");
-    // configure traged server and url
-    // 서버와 url로 연결, http 통신 시작
-    http.begin(client, "http://" SERVER_IP "/postplain/"); //HTTP
-    // 헤더 설정
-    http.addHeader("Content-Type", "application/json");
+  if(now - sensor_time > Time){
+    sensor_time = now;
 
-    Serial.print("[HTTP] POST\n");
-    // start connection and send HTTP header and body
-    // 연결 시작, http header와 body를 전송
-    int httpCode = http.POST("{\"hello\":\"world\"}");
-
-    // httpCode will be negative on error
-    // httpcode는 에러가 났을 때 음수
-    // httpcode가 양수일 때 = 전송과정에서 에러가 발생하지 않음
-    if (httpCode > 0) {
-      // HTTP header has been send and Server response header has been handled
-      // header가 전송되었고, 서버에서 header를 처리함
-      Serial.printf("[HTTP] POSTED code: %d\n", httpCode);
-
-      // file found at server
-      // 서버로부터 파일을 찾음
-      // 전송 상태가 올바르면 작동
-      if (httpCode == HTTP_CODE_OK) {
-        String payload = http.getString();
-        Serial.println("received payload : <<");
-        Serial.println(payload);
-        Serial.println(">>");
-      }
+    Temp = dht.readTemperature();
+    Soil_Data = analogRead(SOIL_SENSOR);
+    if(isnan(Temp)){
+      USE_SERIAL.println("Failed to read from DHT11!");
+      return;
     }
-    // httpcode가 음수일 때 = 전송할 때 에러 발생
-    else {
-      Serial.printf("[HTTP] POST failed, error: %s\n", http.errorToString(httpCode).c_str());
+    if(isnan(Soil_Sensor)){
+      USE_SERIAL.println("Failed to read from Soil_Sensor!");
+      return;
     }
-
-    http.end();
   }
 
-  delay(10000);
+  // 10초마다 실행
+  if(now - messageTimestamp > Time) {
+    messageTimestamp = now;
+
+    // 소켓io를 위한 Json 메시지(String형) 생성
+    DynamicJsonDocument doc(1024);
+    JsonArray array = doc.to<JsonArray>();
+
+    // Json에 이벤트 이름 추가
+    array.add("Data");
+
+    // Json 객체에 문자열 추가
+    JsonObject param1 = array.createNestedObject();
+    param1["now"] = (uint32_t) now;
+
+    // String에 Json 객체가 직렬화되어 저장
+    String output = to_String(Temp);
+    output += ", ";
+    output += to_String(Soil_Data);
+    serializeJson(doc, output);
+
+    // 메시지 전송
+    socketIO.sendEVENT(output);
+
+    // Json 메시지 출력
+    USE_SERIAL.print("[Socket.IO] Send Message : ");
+    USE_SERIAL.println(output);
+  }
 }
